cbf74683f7de475f6ae6e4b19b6e6d98dc412ff7
vadimgupal <114927468+vadimgupal@users.noreply.github.com>
2023-12-12 20:26:25 +0300

    add program in c#

---
+++ TeamCity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+
+namespace TeamCity
+{
+    internal class Program
+    {
+        static int sum(int a,int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            WriteLine(sum(1, 2));
+        }
+    }
+}
